1b5539c659e172113c8d82b2d9e46c24c60c7336
Daya Sharma <daya.sharma+github@gmail.com>
2012-04-25 12:44:01 -0500

    false should be true should fail

--- spec/true_spec.rb
+++ spec/true_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
-describe "something" do
+describe "blah" do
   it "should test true" do
     true.should be_true
+  end 
+
+  it "should test false is true" do
+    false.should be_true
   end 
 end
